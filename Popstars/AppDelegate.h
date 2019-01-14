14eb9537ec7efb52bc4aa91b58bae9d35a329b3a
Roland Tecson <roland_tecson@hotmail.com>
2019-01-13 21:21:54 -0800

    Initial Commit

---
+++ Popstars/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Popstars
+//
+//  Created by Roland on 2019-01-13.
+//  Copyright © 2019 Game of Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Popstars/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Popstars
+//
+//  Created by Roland on 2019-01-13.
+//  Copyright © 2019 Game of Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
